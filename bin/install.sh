72e122822cb6fdc14ddd4039c55f9cb972b45c53
Lawrence Ching <lawrence.ching.zh@gmail.com>
2024-01-27 18:38:40 +0800

    fix installation failure

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,4 @@
-mdkir -p /etc/sj/
-curl -O https://github.com/lawrenceching/sj/releases/download/v0.0.1/sj-1.0-SNAPSHOT.jar --output-dir /etc/sj/
-curl https://raw.githubusercontent.com/lawrenceching/sj/main/bin/sj > /usr/bin/sj
-chmod +x sj
+mkdir -p /etc/sj/
+curl -L https://github.com/lawrenceching/sj/releases/download/v0.0.1/sj-1.0-SNAPSHOT.jar -o /etc/sj/sj.jar
+curl -L https://raw.githubusercontent.com/lawrenceching/sj/main/bin/sj > /usr/bin/sj
+chmod +x /usr/bin/sj
